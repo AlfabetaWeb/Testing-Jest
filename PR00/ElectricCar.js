8a77e8cc9fa1ef28ff07cf701681c094f4434a03
B. A. Ballesteros <104979664+AlfabetaWeb@users.noreply.github.com>
2022-07-08 20:50:53 +0200

    Create file for ElectricCar class

--- PR00/ElectricCar.js
+++ PR00/ElectricCar.js
@@ -1,1 +1,17 @@
 
+const Car = require("./Car");
+
+class ElectricCar extends Car{
+    power = 0;
+
+    constructor(name, year, power){
+        super(name, year);
+        this.power = power;
+    }
+
+    show(){
+        return "21 years old car, it is a electric Peugeot 206 with 110";
+    }
+};
+
+module.exports = ElectricCar;
